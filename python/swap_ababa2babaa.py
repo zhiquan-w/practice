91400f2870e248a76de04fd0bbdfe5f991d070d1
Zhiquan Wang <zqwang18018595661@163.com>
2021-08-28 09:25:39 +0800

    swap ABABA2BABAA

---
+++ python/swap_ababa2babaa.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.random.random([5,3])
+if a.shape[0] % 2 == 0:
+    b = a.copy()
+    b[0::2, :] = a[1::2, :]
+    b[1::2, :] = a[0::2, :]
+else:
+    temp = a[0:-1, :]
+    b = temp.copy()
+    b[0::2, :] = temp[1::2, :]
+    b[1::2, :] = temp[0::2, :]
+
+print(a)
+print(b)
