35f3fb9445d980e6cb96c40edc52267bf7c72ee8
zhiquan <1107173739@qq.com>
2021-12-23 06:54:24 +0000

    l_r_swap

---
+++ cpp/l_r_move.cpp
@@ -0,0 +1,21 @@
+#include <string>
+void swap(std::string & a, std::string & b)
+{
+	std::string temp = std::move(a);
+	a = std::move(b);
+	b = std::move(temp);
+}
+
+#include <iostream>
+
+int main()
+{
+	std::string a = "abc";
+	std::string b = "def";
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	swap(a, b);
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	return 0;
+}
