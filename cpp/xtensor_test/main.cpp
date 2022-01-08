e41fd50456a09fb3c8cd4af2d6df594897437732
zhiquan <1107173739@qq.com>
2022-01-08 08:34:06 +0000

    conv

--- cpp/xtensor_test/main.cpp
+++ cpp/xtensor_test/main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 #include "xtensor/xarray.hpp"
 #include "xtensor/xio.hpp"
+#include "xtensor/xslice.hpp"
+#include "xtensor/xview.hpp"
+#include "ops/conv.hpp"
 
-int main()
-{
-	xt::xarray<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9};
+int main(){
+    xt::xarray<double> a1
+    {
+    {
+        {
+            {2, 3},
+            {3, 4},
+        },
+    },
+    {
+        {
+            {2, 1},
+            {3, 2},
+        },
+    }
+    };
 
-	arr.reshape({3, 3});
+    // (3,1,2,2)
+    xt::xarray<double> a2
+    {
+    {
+        {
+            {2, 1},
+            {1, 2},
+        }
+    },
+    {
+        {
+            {3, 4},
+            {3, 2},
+        }
+    },
+    {
+        {
+            {3, 5},
+            {5, 0},
+        }
+    }
+    };
 
-	std::cout << arr << std::endl;
-	return 0;
+    auto&& x = conv2d(a1, a2, 1, 1);
+    std::cout << x << std::endl;
 }
