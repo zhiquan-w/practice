fcd65c8890864e0fbfecb5bc915ccb90a2cdf082
zhiquan <1107173739@qq.com>
2022-01-08 09:49:40 +0000

    rw_file

---
+++ cpp/sechdule_simulator/modules.h
@@ -0,0 +1,28 @@
+enum process_type
+{
+    CONV,
+    CV,
+    MV
+};
+class conv
+{
+    int process_type;
+    
+    int cost_time() {
+        
+    }
+    /* data */
+};
+
+struct cv
+{
+    int process_type;
+    /* data */
+};
+
+struct teng
+{
+    int process_type;
+    /* data */
+};
+
